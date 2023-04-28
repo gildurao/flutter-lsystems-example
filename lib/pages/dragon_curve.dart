31dd7b7419cb5c9eb3d98bda984bd737dbf1cd35
Gil Durão <43495596+gildurao@users.noreply.github.com>
2023-04-28 10:50:21 +0100

    feat(audio): add musical parsers

--- lib/pages/dragon_curve.dart
+++ lib/pages/dragon_curve.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: implementation_imports
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sequencer/models/instrument.dart';
+import 'package:flutter_sequencer/sequence.dart';
 import 'package:flutter_turtle/flutter_turtle.dart';
 import 'package:lindenmayer_systems/src/lindenmayer_systems/dol_system/dol_system.dart';
 import 'package:lindenmayer_systems/src/production_rules/production_rule.dart';
@@ -13,6 +17,30 @@ final dragonCurve = DolSystem(
     ProductionRule(predecessor: 'Y', successor: '-FX-Y'),
   },
 );
+
+const midiCSharpScale = [
+  49,
+  51,
+  52,
+  54,
+  56,
+  58,
+  60,
+  61,
+  63,
+  64,
+  66,
+  68,
+  70,
+  72,
+  73,
+  75,
+  76,
+  78,
+  80,
+  82,
+  84,
+];
 
 class DragonCurve extends StatefulWidget {
   const DragonCurve({
@@ -42,17 +70,108 @@ class _DragonCurveState extends State<DragonCurve> {
       if (character == '-') commands.add(Right((_) => 90.0));
       if (character == '+') commands.add(Left((_) => 90.0));
     }
+    final sequence = Sequence(
+      tempo: 120,
+      endBeat: sentence.length.toDouble(),
+    );
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            sequence.pause();
+            sequence.stop();
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.close,
+          ),
+        ),
         title: const Text('Dragon Curve'),
         actions: <Widget>[
           TextButton(
             onPressed: () => setState(() {}),
             child: const Text(
-              'Run',
+              'Animation',
               style: TextStyle(color: Colors.white),
             ),
-          )
+          ),
+          TextButton(
+            onPressed: () async {
+              final instrument = SfzInstrument(
+                path: 'assets/sfz/GMPiano.sfz',
+                isAsset: true,
+                tuningPath: "assets/sfz/meanquar.scl",
+              );
+              await sequence.createTracks(
+                [
+                  instrument,
+                ],
+              );
+              int firstVoice = midiCSharpScale.last;
+              int secondVoice = midiCSharpScale.first;
+              double firstVoiceCurrentBeat = 0.0;
+              double secondVoiceCurrentBeat = 0.0;
+              final firstVoiceTrack = sequence.getTracks().first;
+              final secondVoiceTrack = sequence.getTracks().last;
+              const firstVoiceVelocity = 1.0;
+              const secondVoiceVelocity = 0.5;
+              for (int i = 0; i < sentence.length; i++) {
+                String character = sentence[i];
+                if (character == 'F') {
+                  firstVoiceTrack.addNote(
+                    noteNumber: firstVoice,
+                    velocity: firstVoiceVelocity,
+                    startBeat: firstVoiceCurrentBeat,
+                    durationBeats: 0.5,
+                  );
+                  if (i % 2 == 0) {
+                    firstVoiceCurrentBeat += 0.25;
+                  } else {
+                    firstVoiceCurrentBeat += 0.5;
+                  }
+                  secondVoiceTrack.addNote(
+                    noteNumber: secondVoice,
+                    velocity: secondVoiceVelocity,
+                    startBeat: secondVoiceCurrentBeat,
+                    durationBeats: 1.0,
+                  );
+                  secondVoiceCurrentBeat += 0.5;
+                }
+                if (character == '-') {
+                  if (firstVoice != midiCSharpScale.first) {
+                    firstVoice = midiCSharpScale[
+                        midiCSharpScale.indexOf(firstVoice) - 1];
+                  } else {
+                    firstVoice = midiCSharpScale.first;
+                  }
+                  if (secondVoice != midiCSharpScale.first) {
+                    secondVoice = midiCSharpScale[
+                        midiCSharpScale.indexOf(secondVoice) - 1];
+                  } else {
+                    secondVoice = midiCSharpScale.first;
+                  }
+                }
+                if (character == '+') {
+                  if (firstVoice != midiCSharpScale.last) {
+                    firstVoice = midiCSharpScale[Random().nextInt(20)];
+                  } else {
+                    firstVoice = midiCSharpScale.last;
+                  }
+                  if (secondVoice != midiCSharpScale.last) {
+                    secondVoice = midiCSharpScale[
+                        midiCSharpScale.indexOf(secondVoice) + 1];
+                  } else {
+                    secondVoice = midiCSharpScale.last;
+                  }
+                }
+              }
+              sequence.play();
+            },
+            child: const Text(
+              'Music',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ],
       ),
       body: ClipRect(
--- lib/pages/sierpinski_triangle.dart
+++ lib/pages/sierpinski_triangle.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: implementation_imports
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sequencer/models/instrument.dart';
+import 'package:flutter_sequencer/sequence.dart';
 import 'package:flutter_turtle/flutter_turtle.dart';
 import 'package:lindenmayer_systems/src/lindenmayer_systems/dol_system/dol_system.dart';
 import 'package:lindenmayer_systems/src/production_rules/production_rule.dart';
@@ -13,6 +17,8 @@ final sierpsinkiTriangle = DolSystem(
     ProductionRule(predecessor: 'G', successor: 'GG'),
   },
 );
+
+const midiCScale = [60, 62, 64, 65, 67, 69, 71, 72];
 
 class SierpinskiTriangle extends StatefulWidget {
   const SierpinskiTriangle({
@@ -43,14 +49,102 @@ class _SierpinskiTriangleState extends State<SierpinskiTriangle> {
       if (character == '-') commands.add(Right((_) => 120.0));
       if (character == '+') commands.add(Left((_) => 120.0));
     }
+    final sequence = Sequence(
+      tempo: 60,
+      endBeat: sentence.length.toDouble(),
+    );
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            sequence.pause();
+            sequence.stop();
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.close,
+          ),
+        ),
         title: const Text('Sierpinski Triangle'),
         actions: <Widget>[
           TextButton(
             onPressed: () => setState(() {}),
             child: const Text(
-              'Run',
+              'Animation',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final instrument = SfzInstrument(
+                path: 'assets/sfz/GMPiano.sfz',
+                isAsset: true,
+                tuningPath: "assets/sfz/meanquar.scl",
+              );
+              await sequence.createTracks(
+                [
+                  instrument,
+                ],
+              );
+              int firstVoice = midiCScale.first;
+              int secondVoice = midiCScale[2];
+              double firstVoiceCurrentBeat = 0.0;
+              double secondVoiceCurrentBeat = 0.0;
+              final firstVoiceTrack = sequence.getTracks().first;
+              final secondVoiceTrack = sequence.getTracks().last;
+              const firstVoiceVelocity = 1.0;
+              const secondVoiceVelocity = 0.5;
+              for (int i = 0; i < sentence.length; i++) {
+                String character = sentence[i];
+                if (character == 'F') {
+                  firstVoiceTrack.addNote(
+                    noteNumber: firstVoice,
+                    velocity: firstVoiceVelocity,
+                    startBeat: firstVoiceCurrentBeat,
+                    durationBeats: 0.5,
+                  );
+                  firstVoiceCurrentBeat += 1.0;
+                }
+                if (character == 'G') {
+                  secondVoiceTrack.addNote(
+                    noteNumber: secondVoice,
+                    velocity: secondVoiceVelocity,
+                    startBeat: secondVoiceCurrentBeat,
+                    durationBeats: 1.0,
+                  );
+                  secondVoiceCurrentBeat += 0.5;
+                }
+                if (character == '-') {
+                  if (firstVoice != midiCScale.first) {
+                    firstVoice = midiCScale[midiCScale.indexOf(firstVoice) - 1];
+                  } else {
+                    firstVoice = midiCScale.first;
+                  }
+                  if (secondVoice != midiCScale.first) {
+                    secondVoice =
+                        midiCScale[midiCScale.indexOf(secondVoice) - 2];
+                  } else {
+                    secondVoice = midiCScale[2];
+                  }
+                }
+                if (character == '+') {
+                  if (firstVoice != midiCScale.last) {
+                    firstVoice = midiCScale[Random().nextInt(8)];
+                  } else {
+                    firstVoice = midiCScale.last;
+                  }
+                  if (secondVoice != midiCScale.last) {
+                    secondVoice =
+                        midiCScale[midiCScale.indexOf(secondVoice) + 2];
+                  } else {
+                    secondVoice = midiCScale[5];
+                  }
+                }
+              }
+              sequence.play();
+            },
+            child: const Text(
+              'Music',
               style: TextStyle(color: Colors.white),
             ),
           )
